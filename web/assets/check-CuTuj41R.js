e63f8978580882f9c483992be39f1b7c57fb85d0
iEZELL <iEZELL@localhost>
2026-07-25 05:09:04 +0800

    feat: redesign usage log detail with meta row, timeline, request/response panels

--- web/assets/check-CuTuj41R.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index-CR6OA8eF.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- web/assets/chevron-down-DIDeB321.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index-CR6OA8eF.js";var t=e(`ChevronDown`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
