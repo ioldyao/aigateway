964d81de378d0f233417900c7195fb34a1945059
iEZELL <iEZELL@localhost>
2026-07-25 06:43:04 +0800

    fix: remove admin view suffix from request logs subtitle

---
+++ web/assets/chevron-down-BY51migQ.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-bmmZn99L.js";var t=e(`ChevronDown`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
--- web/assets/chevron-down-ndm1KZro.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index-r8CWx7He.js";var t=e(`ChevronDown`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
